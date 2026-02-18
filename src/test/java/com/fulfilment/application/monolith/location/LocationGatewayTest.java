6aa8ccdcaaffed106b523b4bfb58577346fca228
vinnusura <vardhan.sura18@gmail.com>
2026-02-18 15:20:11 +0530

    Add files via upload

---
+++ src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -0,0 +1,18 @@
+package com.fulfilment.application.monolith.location;
+
+import org.junit.jupiter.api.Test;
+
+public class LocationGatewayTest {
+
+  @Test
+  public void testWhenResolv                            eExistingLocationShouldReturn() {
+    // given
+    // LocationGateway locationGateway = new LocationGateway();
+
+    // when
+    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+
+    // then
+    // assertEquals(location.identification, "ZWOLLE-001");
+  }
+}
